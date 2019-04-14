8825b5cbc771b3000be022fef24a739afe7472a2
BrianMichell <ecwhodie@yahoo.com>
2019-04-14 17:30:44 -0500

    Initial commit of base classes.

---
+++ Supplier.java
@@ -0,0 +1,11 @@
+class Supplier extends User {
+
+    public Supplier() {
+        super();
+    }
+
+    public boolean createAccount() {
+        return false;
+    }
+
+}
---
+++ System.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+class System {
+
+    private ArrayList <User> currentUsers;
+
+    public System(){
+
+    }
+
+    /**
+     * @param User The user that wishes to log in
+     * @return If the user object supplied was successfuly logged in
+     */
+    public boolean logIn(User user) {
+        return true;
+    }
+
+    /**
+     * @param User The user that wishes to log out
+     * @return If the user object supplied was suffessfuly logged out
+     */
+    public boolean logOut(User user) {
+        return true;
+    }
+
+
+}
