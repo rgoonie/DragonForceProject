4a27101516e4e58013299b9773696efdebe1bd15
BrianMichell <ecwhodie@yahoo.com>
2019-04-15 16:00:34 -0500

    Removed customer object from class.

--- src/Order.java
+++ src/Order.java
@@ -3,14 +3,12 @@ class Order {
     private String status;
     private double authNumber, total;
     private Cart cart;
-    private Customer customer;
 
-    public Order(Cart cart, double total, Customer customer, Bank bank) {
+    public Order(Cart cart, String cardNumber, Bank bank) {
         this.status = "PENDING";
-        this.total = total;
+        this.total = cart.getTotal();
         this.cart = cart;
-        this.customer = customer;
-        this.authNumber = bank.processOrder(customer.getCardNumber(), cart.getTotal());
+        this.authNumber = bank.processOrder(cardNumber, cart.getTotal());
     }
 
     public Cart getCart() {
@@ -23,10 +21,6 @@ class Order {
 
     public void setStatus(String newStatus) {
         this.status = newStatus;
-    }
-
-    public Customer getCustomer() {
-        return this.customer;
     }
 
     public double getAuthNumber() {
