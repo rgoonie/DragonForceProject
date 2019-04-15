40ba47812652e82cab5b8a3a6d2edf8f8997a4ba
BrianMichell <ecwhodie@yahoo.com>
2019-04-15 15:36:49 -0500

    Added appropriate getters and setters.

--- src/Item.java
+++ src/Item.java
@@ -2,15 +2,33 @@ class Item {
 
     private double price;
     private String name, description;
+    private int amount;
 
-    public Item(Double price, String name, String description) {
+    public Item(Double price, String name, String description, int amount) {
         this.price = price;
         this.name = name;
         this.description = description;
+        this.amount = amount;
     }
 
     public double getPrice() {
         return this.price;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void updateAmount(int newAmount) {
+        this.amount = newAmount;
     }
 
 }
