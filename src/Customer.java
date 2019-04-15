57b0025c58d6860e1ea71acefde80ea77ae3437c
BrianMichell <ecwhodie@yahoo.com>
2019-04-15 16:01:57 -0500

    Added bank object to constructor, implemented methods.

--- src/Customer.java
+++ src/Customer.java
@@ -1,22 +1,30 @@
+import java.util.ArrayList;
+import java.util.Map;
+
 class Customer extends User {
 
     private String phoneNumber, address, name, creditCardNumber;
-    private Cart cart;
+    private Cart cart; 
+    private ArrayList<Order> orders;
+    private Bank bank;
 
-    public Customer(String id, String password, String phoneNumber, String address, String name, String creditCardNumber) {
+    public Customer(String id, String password, String phoneNumber, String address, String name, String creditCardNumber, Bank bank) {
         super(id, password);
         this.phoneNumber = phoneNumber;
         this.address = address;
         this.name = name;
         this.creditCardNumber = creditCardNumber;
         this.cart = new Cart();
+        this.orders = new ArrayList<Order>();
+        this.bank = bank;
     }
 
     /**
-     * @return If the item was successfully added
+     * Places the order and empties the cart
      */
-    public boolean orderItems() {
-        return false;
+    public void orderItems() {
+        orders.add(new Order(this.cart, this.creditCardNumber, this.bank));
+        this.cart = new Cart();
     }
 
     public boolean createAccount() {
@@ -26,8 +34,17 @@ class Customer extends User {
     /**
      * @return All the items in the users cart
      */
-    public String viewItems(Order order) {
-        return "";
+    public String viewItems() {
+        String ret = "";
+        //Iterate over evey item in cart and add line return at the end
+        for(Map.Entry<Item, Double> item: cart.getItems().entrySet()) {
+            ret += item.getKey() + "\n";
+        }
+        return ret;
+    }
+
+    public String getCardNumber() {
+        return this.creditCardNumber;
     }
 
 }
