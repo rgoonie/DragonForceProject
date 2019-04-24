0c2ab52de88003165e7d6d5b494440239d3d3ac8
BrianMichell <ecwhodie@yahoo.com>
2019-04-24 17:23:36 -0500

    Added quantity instance variable and appropriate getter/setter.

--- src/Item.java
+++ src/Item.java
@@ -5,11 +5,13 @@ class Item implements Serializable {
 
     private double price;
     private String name, description;
+    private int quantity;
 
-    public Item(String name, String description, double price) {
+    public Item(String name, String description, double price, int quantity) {
         this.price = price;
         this.name = name;
         this.description = description;
+        this.quantity = quantity;
     }
 
     public double getPrice() {
@@ -24,5 +26,21 @@ class Item implements Serializable {
         return this.description;
     }
     
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    /**
+     * Removes a selected quantity of items if avaliable
+     * @param num The number of the item to be removed from inventory
+     * @return True if there were enough in stock. If true is returned the quantity was successfuly removed.
+     */
+    public boolean subtractQuantity(int num) {
+        if(num > this.quantity) {
+            return false;
+        }
+        this.quantity -= num;
+        return true;
+    }
 
 }
