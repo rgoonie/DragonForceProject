986e77ed3dce724cf02bc4213e401c5649f8782e
rgoonie <rajeev.goonie98@gmail.com>
2019-04-24 14:38:07 -0500

    add menu/add log in, create account functions

--- src/User.java
+++ src/User.java
@@ -1,7 +1,18 @@
-abstract class User {
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+abstract class User implements Serializable {
 
     protected String id, password;
 
+    public User(){
+        this.id = "";
+        this.password = "";
+    }
+    
     public User(String id, String password) {
         this.id = id;
         this.password = password;
@@ -9,8 +20,94 @@ abstract class User {
 
     /**
      * Creates a new User account
-     * @return If the account was successfuly created
+     * Same for all user since only someone not signed in will use this
      */
-    abstract boolean createAccount();
+    final void createAccount(Scanner in, HashMap<String, String> signInInfo, HashMap<String, Customer> customerAccount, HashMap<String, ArrayList<Order>> orderData){
+        char choice = 'X';
+        while(choice != 'c' && choice != 's'){
+            System.out.print("\nAre you a [c]ustomer or [s]upplier:: ");
+            String input = in.nextLine().replace(" ", "").toLowerCase();
+            while(input.equals(""))
+                input += in.nextLine().replace(" ", "").toLowerCase();
+            choice = input.charAt(0);            
+        }
+        
+        System.out.print("Enter an ID (case sensitive):: ");
+        String newID = in.nextLine();
+        while( signInInfo.get(newID) != null ){
+            System.out.println("\nThis id is already taken... Please Try Again");
+            System.out.print("Enter an ID (case sensitive):: ");
+            newID = in.nextLine();
+        }
+        
+        System.out.print("Enter a password (case sensitive, at least 8 characters):: ");
+        String pass = in.nextLine();
+        while(pass.length() < 8){
+            System.out.println("\nThis password is not long enough... Please Try Again");
+            System.out.print("Enter a password (case sensitive, at least 8 characters):: ");
+            pass = in.nextLine();
+        }
+        
+        
+        if(choice == 'c'){
+            System.out.print("Enter your name:: ");
+            String name = in.nextLine();
+            
+            String phone = "";
+            while(phone.equals("")){
+                System.out.print("Enter your phone number:: ");
+                phone = in.nextLine().replace('(', ' ').replace(')', ' ').replace('-', ' ').replace(" ", "");
+                
+                try{Long.parseLong(phone);}
+                catch(Exception e){
+                    System.out.println("The phone number is not vaild... Please Try Again\n");
+                    phone = "";
+                }
+            }
+            
+            System.out.print("Enter your address:: ");
+            String address = in.nextLine().replace(" ", "").toUpperCase();
+            while( address.equals("") )
+                address += in.nextLine().replace(" ", "").toUpperCase();
+            
+            String credit = "";
+            while(credit.equals("")){
+                System.out.print("Enter your credit card number:: ");
+                credit = in.nextLine().replace('-', ' ').replace(" ", "");
+                
+                try{Long.parseLong(credit);}
+                catch(Exception e){
+                    System.out.println("The credit card number is not vaild... Please Try Again\n");
+                    credit = "";
+                }
+            }
+            
+            signInInfo.put(newID, pass);
+            customerAccount.put( newID, new Customer(newID, pass, phone, address, name, credit) );
+            orderData.put(newID, new ArrayList<Order>());
+        }
+        
+        else{
+            signInInfo.put(newID, pass);
+        }
+        
+    }
 
+    /**
+     * Sign in existing User
+     * Same for all users as both suppliers and customers need to have id and pass
+     */
+    final void signIn(Scanner in, String[] out){
+        System.out.print("\nEnter User ID:: ");
+        out[0] = in.nextLine();
+        System.out.print("Enter Password:: ");
+        out[1] = in.nextLine();
+    }
+    
+    /**
+     * Runs menu logic for user
+     * @return command chosen by user
+     */
+    abstract int menu(Scanner in);
+    
 }
