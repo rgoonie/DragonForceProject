c62acc855a79184621ea484b6020dbc38d6760f3
rgoonie <rajeev.goonie98@gmail.com>
2019-04-24 14:28:44 -0500

    add, change fields/add serialize/add toString

--- src/Order.java
+++ src/Order.java
@@ -1,18 +1,23 @@
-class Order {
 
-    private String status;
-    private double authNumber, total;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+class Order implements Serializable{
+
+    private String status, name, address, phone, authNumber;
+    private Date date;
     private Cart cart;
 
-    public Order(Cart cart, String cardNumber, Bank bank) {
+    public Order(Cart cart, Date orderDate, String name, String phone, String address) {
         this.status = "PENDING";
-        this.total = cart.getTotal();
         this.cart = cart;
-        this.authNumber = bank.processOrder(cardNumber, cart.getTotal());
-    }
-
-    public Cart getCart() {
-        return this.cart;
+        this.date = orderDate;
+        
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
     }
 
     public String getStatus() {
@@ -23,12 +28,36 @@ class Order {
         this.status = newStatus;
     }
 
-    public double getAuthNumber() {
+    public String getAuthNumber() {
         return this.authNumber;
     }
 
     public double getTotal() {
-        return this.total;
+        return this.cart.getTotal();
     }
+    
+    public void viewOrder(){
+        HashMap<Item, Integer> items = cart.getItems();
+        
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Order Made On: " + dateFormat.format(date) + "\n");
+        System.out.println("Status:        " + status + "\n");
+        
+        System.out.println( String.format("%-15s%15s%15s", "<Items>", "<Quantity>", "<Cost>") );
+        for(Item item : items.keySet()){
+            System.out.println( String.format("%-15s%15d%15.2f", item.getName(), items.get(item), item.getPrice()*items.get(item)) );
+        }
+        System.out.println( String.format("\n%-15s%15s%15.2f", "Total Cost.....", "...............", cart.getTotal()) );
+        System.out.println("--------------------------------------------------");
+        System.out.println("\n");
+        
+    }
+    
+    public String toString(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        return "Order from " + dateFormat.format(date);
+    }
+    
 
 }
