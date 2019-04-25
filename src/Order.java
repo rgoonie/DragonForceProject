9f40e10d6beef9a2ad9f8bafb4aaf10b4e87cb0e
rgoonie <rajeev.goonie98@gmail.com>
2019-04-25 16:27:14 -0500

    updated Order Display

--- src/Order.java
+++ src/Order.java
@@ -43,7 +43,7 @@ class Order implements Serializable {
         SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
         System.out.println("--------------------------------------------------");
         System.out.println("Order Made On: " + dateFormat.format(date) + "\n");
-        System.out.println("Status:        " + status + "\n");
+        System.out.println("Status: " + status + "\n");
         
         System.out.println( String.format("%-15s%15s%15s", "<Items>", "<Quantity>", "<Cost>") );
         for(Item item : items.keySet()) {
@@ -51,7 +51,6 @@ class Order implements Serializable {
         }
         System.out.println( String.format("\n%-15s%15s%15.2f", "Total Cost.....", "...............", cart.getTotal()) );
         System.out.println("--------------------------------------------------");
-        System.out.println("\n");
         
     }
     
